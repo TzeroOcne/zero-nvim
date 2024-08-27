1b15319460a9e68cee5f666530f71b14c1911cdb
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-08-27 16:53:49 +0700

    feat(telescope): add telescope key

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -3,5 +3,16 @@ return {
   -- tag = '0.1.8',
   -- or
   -- branch = '0.1.x',
-  dependencies = { 'nvim-lua/plenary.nvim' }
+  event = "VeryLazy",
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  keys = function ()
+    local builtin = require('telescope.builtin')
+    return {
+      { '<leader>f/', builtin.live_grep, desc = "Live grep" },
+      { '<leader>f:', builtin.command_history, desc = "Find command" },
+      { '<leader>fb', builtin.buffers, desc = "Find buffers" },
+      { '<leader>ff', builtin.find_files, desc = "Find files" },
+      { '<leader>fg', builtin.git_files, desc = "Find git files" },
+    }
+  end,
 }
--- lua/plugins/which-key.lua
+++ lua/plugins/which-key.lua
@@ -1,10 +1,23 @@
 return {
   "folke/which-key.nvim",
   event = "VeryLazy",
+  ---@type wk.Opts
   opts = {
     -- your configuration comes here
     -- or leave it empty to use the default settings
-    -- refer to the configuration section below
+    -- refer to the configuration section below,
+    defaults = {},
+    icons = {
+      group = "",
+    },
+    spec = {
+      mode = { "n", "v" },
+      {
+        "<leader>f",
+        desc = "Telescope Find",
+        icon = {},
+      },
+    },
   },
   keys = {
     {
