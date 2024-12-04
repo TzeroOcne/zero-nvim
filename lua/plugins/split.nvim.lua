4056aeec6cdbd879fe284c4b91f6a9fd8df25ed7
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-12-04 16:26:52 +0700

    feat(plugins): add split.nvim plugin

---
+++ lua/plugins/split.nvim.lua
@@ -0,0 +1,29 @@
+return {
+  "wurli/split.nvim",
+  opts = {
+    keymaps = {
+      -- Other keymaps are available :) these ones will be used
+      -- by default.
+      ["gl"] = {
+        pattern = ",",
+        operator_pending = true,
+        interactive = false,
+      },
+      ["gls"] = {
+        pattern = ",",
+        operator_pending = false,
+        interactive = false,
+      },
+      ["gL"] = {
+        pattern = ",",
+        operator_pending = true,
+        interactive = true,
+      },
+      ["gLS"] = {
+        pattern = ",",
+        operator_pending = false,
+        interactive = true,
+      },
+    },
+  },
+}
