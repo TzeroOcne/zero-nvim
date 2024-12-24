1da17ae0e258c466fb65d87e14ed5eec6d1a2574
Tzero Ocne <tzeroocne.interface@gmail.com>
2024-12-24 14:50:34 +0700

    feat(outline): add open and close outline key

--- lua/plugins/outline.lua
+++ lua/plugins/outline.lua
@@ -3,9 +3,15 @@ return {
   lazy = true,
   cmd = { "Outline", "OutlineOpen" },
   keys = { -- Example mapping to toggle outline
-    { "<leader>lo", "<cmd>Outline<CR>", desc = "Toggle outline" },
+    { "<leader>lo", "<cmd>OutlineOpen<CR>", desc = "Toggle outline" },
+    { "<leader>lO", "<cmd>OutlineClose<CR>", desc = "Toggle outline" },
   },
   opts = {
     -- Your setup opts here
+    outline_window = {
+      -- Vim options for the outline window
+      show_numbers = true,
+      show_relative_numbers = true,
+    },
   },
 }
