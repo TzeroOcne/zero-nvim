f23c20bed277659c41a439b6b60715ad6d13d211
Tzero Ocne <tzeroocne.interface@gmail.com>
2025-02-03 13:50:07 +0700

    feat(float): make float window transparent

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -62,18 +62,22 @@ vim.cmd([[
 ]])
 
 vim.api.nvim_set_hl(0, 'Comment', { fg='#909090' })
+-- NormalFloat   xxx guifg=#c0caf5 guibg=#1a1b26
+vim.api.nvim_set_hl(0, 'NormalFloat', { link = 'none', bg = 'none' })
+-- FloatTitle    xxx guifg=#27a1b9 guibg=#1a1b26
+vim.api.nvim_set_hl(0, 'FloatTitle', { link = 'none', bg = 'none' })
 vim.api.nvim_set_hl(0, 'DiagnosticUnnecessary', { fg='#909090' })
 -- FloatBorder   xxx guifg= guibg=#1a1b26
 vim.api.nvim_set_hl(0, 'FloatBorder', { fg = '#27a1b9', bg = 'none' })
-vim.api.nvim_set_hl(0, 'LineNrAbove', { fg= '#009090', bold=true })
-vim.api.nvim_set_hl(0, 'LineNrBelow', { fg= '#009090', bold=true })
-vim.api.nvim_set_hl(0, 'LineNr', { fg= 'Cyan' })
-vim.api.nvim_set_hl(0, 'WinSeparator', { fg= '#0080b0' })
+vim.api.nvim_set_hl(0, 'LineNrAbove', { fg = '#009090', bold = true })
+vim.api.nvim_set_hl(0, 'LineNrBelow', { fg = '#009090', bold = true })
+vim.api.nvim_set_hl(0, 'LineNr', { fg = 'Cyan' })
+vim.api.nvim_set_hl(0, 'WinSeparator', { fg = '#0080b0' })
 vim.api.nvim_set_hl(0, 'NeoTreeNormal', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'NeoTreeNormalNC', { bg = 'none' })
 vim.api.nvim_set_hl(0, 'SnacksNormal', { link = 'none', bg = 'none' })
 vim.api.nvim_set_hl(0, 'SnacksPicker', { link = 'none', bg = 'none' })
 vim.api.nvim_set_hl(0, 'NeoTreeCursorLine', { link = 'none', bg = 'none' })
 vim.api.nvim_set_hl(0, 'TreesitterContext', { link = 'none', bg = 'none' })
-vim.api.nvim_set_hl(0, 'TreesitterContextBottom', { underline=true, sp= 'grey' })
+vim.api.nvim_set_hl(0, 'TreesitterContextBottom', { underline = true, sp = 'grey' })
 vim.api.nvim_set_hl(0, 'TreesitterContextLineNumberBottom', { underline=true, sp='grey' })
