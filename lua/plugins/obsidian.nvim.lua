0049decc0284ef888a0cb59444018e20a6b0a2c9
Tzero Ocne <tzeroocne.interface@gmail.com>
2025-04-28 15:36:23 +0700

    feat(obsidian): change to use maintained fork

--- lua/plugins/obsidian.nvim.lua
+++ lua/plugins/obsidian.nvim.lua
@@ -1,6 +1,7 @@
 return {
-  "epwalsh/obsidian.nvim",
-  version = "*",  -- recommended, use latest release instead of latest commit
+  "obsidian-nvim/obsidian.nvim",
+  -- version = "*", -- recommended, use latest release instead of latest commit
+  commit = "3c967d0",
   lazy = true,
   ft = "markdown",
   -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
@@ -17,44 +18,11 @@ return {
 
     -- see below for full list of optional dependencies 👇
   },
-  keys = function ()
-    return {
-      -- Overrides the 'gf' mapping to work on markdown/wiki links within your vault.
-      {
-        "<leader>of",
-        function()
-          return require("obsidian").util.gf_passthrough()
-        end,
-        desc = "Jump link",
-        noremap = false,
-        expr = true,
-        buffer = true,
-      },
-      -- Toggle check-boxes.
-      {
-        "<leader>oc",
-        function()
-          return require("obsidian").util.toggle_checkbox()
-        end,
-        desc = "Interact Check",
-        buffer = true,
-      },
-      -- Smart action depending on context, either follow link or toggle checkbox.
-      {
-        "<leader>oo",
-        function()
-          return require("obsidian").util.smart_action()
-        end,
-        desc = "Interact",
-        buffer = true,
-        expr = true,
-      }
-    }
-  end,
   opts = function ()
     vim.o.conceallevel = 1
 
     return {
+      legacy_commands = false,
       workspaces = {
         {
           name = "buf-parent",
@@ -63,10 +31,24 @@ return {
           end,
         },
       },
+      -- see below for full list of options 👇
 
       -- Optional, boolean or a function that takes a filename and returns a boolean.
       -- `true` indicates that you don't want obsidian.nvim to manage frontmatter.
       disable_frontmatter = true,
+
+      -- Optional, completion of wiki links, local markdown links, and tags using nvim-cmp.
+      completion = {
+        -- Enables completion using nvim_cmp
+        nvim_cmp = false,
+        -- Enables completion using blink.cmp
+        blink = true,
+        -- Trigger completion at 2 chars.
+        min_chars = 2,
+      },
     }
+  end,
+  setup = function (_, opts)
+    require("obsidian").setup(opts)
   end,
 }
