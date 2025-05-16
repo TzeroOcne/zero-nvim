11ad9996a7ae58e6d94c1b831775a5816f536218
Tzero Ocne <tzeroocne.interface@gmail.com>
2025-05-16 13:51:59 +0700

    feat(keymap): add keymap for line navigation

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -81,3 +81,7 @@ map("n", "<C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
 map("n", "<C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
 map("n", "<C-Left>", "<cmd>vertical resize -2<cr>", { desc = "Decrease Window Width" })
 map("n", "<C-Right>", "<cmd>vertical resize +2<cr>", { desc = "Increase Window Width" })
+
+-- Quick line navigation
+map({ "n", "v" }, "<leader>hh", "^", { desc = "Start of line" })
+map({ "n", "v" }, "<leader>hl", "$", { desc = "End of line" })
