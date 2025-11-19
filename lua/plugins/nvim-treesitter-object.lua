2fa7d18d04274ba00db17b4c04e7e4032e4da87a
Tzero Ocne <tzeroocne.interface@gmail.com>
2025-11-19 19:29:59 +0700

    feat(treesitter): add ensure installed

--- lua/plugins/nvim-treesitter-object.lua
+++ lua/plugins/nvim-treesitter-object.lua
@@ -1,31 +1,11 @@
 return {
   'nvim-treesitter/nvim-treesitter-textobjects',
   -- enabled = false,
-  branch = "main",
+  -- branch = "master",
   -- dependencies = {
   --   'nvim-treesitter/nvim-treesitter',
   -- },
-  opts = {
-    select = {
-      enable = true,
-      lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-      keymaps = {
-        -- You can use the capture groups defined in your grammar.
-        ['af'] = '@function.outer',
-        ['if'] = '@function.inner',
-        ['ac'] = '@class.outer',
-        ['ic'] = '@class.inner',
-        ['as'] = '@snippet.outer',
-        ['is'] = '@snippet.inner',
-      },
-      -- You can choose the select mode (default is charwise 'v')
-      selection_modes = {
-        ['@parameter.outer'] = 'v', -- charwise
-        ['@function.outer'] = 'V', -- linewise
-        ['@class.outer'] = '<c-v>', -- blockwise
-      },
-    },
-  },
+  opts = {},
   config = function()
     require('nvim-treesitter.configs').setup({
       textobjects = {
