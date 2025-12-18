737cdd689301f6dc1bf897446f9fb7d4804aa970
Tzero Ocne <tzeroocne.interface@gmail.com>
2025-12-18 16:46:12 +0700

    feat(lazydev): add wezterm type

--- lua/plugins/lazydev.lua
+++ lua/plugins/lazydev.lua
@@ -1,15 +1,21 @@
 return {
-  {
-    "folke/lazydev.nvim",
-    ft = "lua", -- only load on lua files
-    opts = {
-      library = {
-        -- See the configuration section for more details
-        -- Load luvit types when the `vim.uv` word is found
-        { path = "luvit-meta/library", words = { "vim%.uv" } },
-      },
+  "folke/lazydev.nvim",
+  ft = "lua", -- only load on lua files
+  dependencies = {
+    {
+      'DrKJeff16/wezterm-types',
+      lazy = true,
+      version = false, -- Get the latest version
+    },
+    { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+    -- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
+  },
+  opts = {
+    library = {
+      -- See the configuration section for more details
+      -- Load luvit types when the `vim.uv` word is found
+      { path = "luvit-meta/library", words = { "vim%.uv" } },
+      { path = 'wezterm-types', mods = { 'wezterm' } },
     },
   },
-  { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
-  -- { "folke/neodev.nvim", enabled = false }, -- make sure to uninstall or disable neodev.nvim
 }
