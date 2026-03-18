4a04e7956caf146d0023d89bdc847e99430bb482
Tzero Ocne <tzeroocne.interface@gmail.com>
2026-03-18 14:48:20 +0700

    feat(copilot): enable copilot inside yaml file

--- lua/plugins/copilot.lua
+++ lua/plugins/copilot.lua
@@ -1,5 +1,6 @@
 return {
   "zbirenbaum/copilot.lua",
+  enabled = false,
   cmd = "Copilot",
   build = ":Copilot auth",
   event = "InsertEnter",
@@ -9,6 +10,9 @@ return {
         enabled = false,
       },
       panel = { enabled = false },
+      filetypes = {
+        yaml = true,
+      },
     }
     local exe = vim.fn.exepath("copilot-language-server")
     if exe ~= "" then
