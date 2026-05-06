6d71e558db99b7c87729986b279cfe42821d47cd
Tzero Ocne <tzeroocne.interface@gmail.com>
2026-05-06 19:56:55 +0700

    feat(obsidian): change lazy load to detect obsidian vault instead

--- lua/plugins/obsidian.nvim.lua
+++ lua/plugins/obsidian.nvim.lua
@@ -4,15 +4,13 @@ return {
   -- tag = "~v3.14.7",
   -- version = "3.15.0",
   lazy = true,
-  ft = "markdown",
-  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
-  -- event = {
-  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
-  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
-  --   -- refer to `:h file-pattern` for more examples
-  --   "BufReadPre path/to/my-vault/*.md",
-  --   "BufNewFile path/to/my-vault/*.md",
-  -- },
+  event = function()
+    if require("zero").is_obsidian_project() then
+      return { "VimEnter", "BufReadPre", "BufNewFile" }
+    end
+
+    return {}
+  end,
   dependencies = {
     -- Required.
     "nvim-lua/plenary.nvim",
